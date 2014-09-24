78bdb0bb71de8dd8b7f834389baf2b6f3232ebab
Rachel <rmoshier@gmail.com>
2014-09-24 13:30:54 -0700

    first bullet and 8 tests

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -21,6 +21,10 @@ describe Scrabble do
   end
 
   describe "#self.highest_score_from" do
+    it "responds to 'highest_score'" do
+      expect(Scrabble).to respond_to :highest_score_from
+    end
+
     it "it returns the word in the array with the highest score" do
       expect(Scrabble.highest_score_from(["cat", "mat", "kat"])).to eq "kat"
     end
@@ -30,4 +34,9 @@ describe Scrabble do
     end
   end
 
+  describe "#self.count_words" do
+    it "responds to 'count_words'" do
+      expect(Scrabble). to respond_to :count_words
+    end
+  end
 end
